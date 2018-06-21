27c364a2cecd044660de20dc0e939370efe6ad89
gorankunjadic <goran.kunjadic@abcrypto.com>
2018-06-21 22:16:24 +0200

    Add files via upload

---
+++ App1/ViewController.cs
@@ -0,0 +1,33 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace App1
+{
+    public partial class ViewController : UIViewController
+    {
+        //public System.Threading.Tasks.Task DisplayAlert(string title, string message, string cancel);
+
+        public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+        partial void Dugme_TouchUpInside(UIButton sender)
+        {
+            //System.Threading.Tasks.Task DisplayAlert(string title, string message, string cancel);
+           // DisplayAlert("Alert", "You have been alerted", "OK");
+        }
+    }
+}
---
+++ App1/ViewController.designer.cs
@@ -0,0 +1,42 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace App1
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Dugme { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel Napisano { get; set; }
+
+        [Action ("Dugme_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Dugme_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (Dugme != null) {
+                Dugme.Dispose ();
+                Dugme = null;
+            }
+
+            if (Napisano != null) {
+                Napisano.Dispose ();
+                Napisano = null;
+            }
+        }
+    }
+}
